f9a6d1c9c6cd65ac9d32e8496bbc33273a937020
CodeByLimaSantos <guilimadev19@gmail.com>
2026-03-09 19:45:21 -0300

    first commit

---
+++ src/main/java/LimaSantos/SuperStock/SuperStockApplication.java
@@ -0,0 +1,13 @@
+package LimaSantos.SuperStock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperStockApplication.class, args);
+	}
+
+}
